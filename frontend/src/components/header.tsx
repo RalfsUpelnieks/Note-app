68a075d301e8c6f3cab904b5f6bede40943016fe
Ralfs Upelnieks <ralfs.upelnieks@gmail.com>
2023-11-28 09:28:12 +0200

    Added page title search

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,7 +1,36 @@
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from "react";
 
-function Header({isAdmin} : any) {
+
+function Header({isAdmin, pages}: any) {
     const navigate = useNavigate();
+    const [pageList, setPageList] : any = useState(pages);
+    const [search, setSearch] = useState("");
+    const [selectedTag, setSelectedTag] = useState(0);
+
+
+    useEffect(() => {
+        if(search){
+            var list : any = [];
+            const searchLowercase = search.toLowerCase();
+            const emptyTextPlaceholder = "untitled";
+
+            for (const item of pages){
+                if(item.title.toLowerCase().includes(searchLowercase)) {
+                    list.push(item);
+                } else if (item.title === "" && emptyTextPlaceholder.includes(searchLowercase)) {
+                    list.push(item);
+                }
+            }
+            setPageList(list);
+        } else {
+            setPageList([]);
+        }
+    }, [search]);
+
+    function handleChange(e) {
+        setSearch(e.target.value);
+    };
 
     function logOutUser(){
         localStorage.removeItem('token');
@@ -12,7 +41,24 @@ function Header({isAdmin} : any) {
         <header className='flex fixed left-0 right-0 top-0 bottom-auto px-4 justify-between z-10 items-center bg-[rgb(233,233,233)] shadow-[0px_3px_5px_-1px_rgba(0,0,0,0.2),_0px_1px_14px_0px_rgba(0,0,0,0.12)]'>
             <h1 className='text-3xl my-3'>SwiftNotes</h1>
             {isAdmin ? <h4 className='self-end ml-2 mr-auto mb-4 opacity-70'>admin page</h4>: null}
-            {!isAdmin ? <input className='w-2/6 mx-3 lg:ml-[12%] ' type="text" placeholder="Search..."/> : null}
+            {!isAdmin ? 
+                <div className=' relative w-2/6 mx-3 lg:ml-[12%]'>
+                    <input className='peer' type="text" placeholder="Search..." onChange={handleChange}/>
+                    <div className='hidden peer-focus:flex peer-focus-within:flex hover:flex flex-col w-[calc(100%-2px)] top-8 absolute bg-white rounded-b-[4px] border border-solid border-t-neutral-200 border-[#ced4da]'>
+                    {pageList.length !== 0 ? (
+                        pageList.map((object) => {
+                            return (
+                                <Link to={`/page/${object.pageId}`} className="">
+                                        <span>{object.title.replaceAll("&nbsp;", " ").replaceAll("<br>", " ") || "Untitled"}</span>
+                                </Link>
+                            );
+                        })
+                    ) : (
+                        <div className="px-2 py-1 text-gray-500">No results</div>
+                    )}
+                    </div>
+                </div>
+            : null}
             <button onClick={logOutUser} className='w-24 h-9 font-semibold text-sm bg-zinc-800 text-white hover:bg-black hover:cursor-pointer border-none rounded '>Log out</button>
         </header>
     )
--- frontend/src/components/layout.tsx
+++ frontend/src/components/layout.tsx
@@ -42,7 +42,7 @@ function Layout({pages, setPages, user} : LayoutProps) {
 
     return (
         <div>
-            <Header isAdmin={user?.role == "1"}/>
+            <Header isAdmin={user?.role == "1"} pages={pages}/>
             <SideNav pages={pages} setPages={setPages} isAdmin={user?.role == "1"}/>
             <main className={styles.content}>
                 <Outlet context={[pages, setPages]} />  
